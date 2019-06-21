05f67a1f0bc31c8282954eca2da632f6d025cb6a
Gary Wheeler <gkwheeler40@gmail.com>
2019-06-21 14:18:46 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,7 +2,9 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
  
   attr_accessor :name
  
